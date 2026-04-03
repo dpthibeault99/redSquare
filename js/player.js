16fd848150c717ab2fee0524e8bddec4e88b6d71
dpthibeault99 <dpthibeault@email.neit.edu>
2026-04-03 12:46:02 -0400

    opps

--- js/player.js
+++ js/player.js
@@ -15,9 +15,9 @@ function Player() {
     this.draw = function(){
 
         context.save();
-            context.fillStyle = this.color
-            context.translate(this.x,this.y);
-            context.fillRect(   (-this.width / 2) , (-this.height / 2 ), this.width, this.height   );
+        context.fillStyle = this.color
+        context.translate(this.x,this.y);
+        context.fillRect(   (-this.width / 2) , (-this.height / 2 ), this.width, this.height   );
         context.restore();
 
     }
